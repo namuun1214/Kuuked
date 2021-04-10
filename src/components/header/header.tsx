30cedeb4e4aacd30a09be02822646787a79b4772
namuun1214 <namuun1214@gmail.com>
2021-04-10 19:59:41 +0800

    sidemenu is done

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { TouchableOpacity } from 'react-native';
 import { Margin, Queue } from '..';
 import { KuukedLogoWhite } from '../../assets';
 import { BackArrowIcon, MenuIcon } from '../../assets/icons';
 import { Border, Text } from '../core';
-import { Center, Padding } from '../layout';
+import { Center, Overlay, Padding } from '../layout';
 import { useNavigation } from '@react-navigation/native';
 import Drawer from 'react-native-drawer';
 import { NavigationRoutes } from '../navigation/navigation-param';
@@ -15,52 +15,58 @@ export const Header = ({ withBack = false, headerText = '', menu = true }) => {
   const goBack = () => {
     navigation.goBack();
   };
+  const handleMenuClick = () => {
+    // navigation.navigate(NavigationRoutes.SideMenu);
+    setMenuClicked(!menuClicked);
+  };
   const [menuClicked, setMenuClicked] = useState(false);
   return (
-    <Border backgroundRole="info">
-      <Padding size={[2, 0, 2, 2]}>
-        <Margin size={[2, 1, 0, 2]}>
-          {withBack && (
-            <Queue
-              size={5}
-              alignItems="center"
-              justifyContent={menu ? 'space-between' : 'flex-start'}>
-              <TouchableOpacity onPress={goBack}>
-                <BackArrowIcon width={'10'} height={'40'} role="primary" />
-              </TouchableOpacity>
-              {headerText && (
-                <Text role="primary" type="primaryBody1" textAlign="center">
-                  {headerText}
-                </Text>
-              )}
-              {menu && (
-                <TouchableOpacity
-                  onPress={() => {
-                    setMenuClicked(!menuClicked);
-                  }}>
-                  <MenuIcon />
+    <>
+      <Border backgroundRole="info">
+        <Padding size={[2, 0, 2, 2]}>
+          <Margin size={[2, 1, 0, 2]}>
+            {withBack && (
+              <Queue
+                size={5}
+                alignItems="center"
+                justifyContent={menu ? 'space-between' : 'flex-start'}>
+                <TouchableOpacity onPress={goBack}>
+                  <BackArrowIcon width={'10'} height={'40'} role="primary" />
                 </TouchableOpacity>
-              )}
-            </Queue>
-          )}
-          {!withBack && (
-            <Queue justifyContent="space-between">
-              <Center>
-                <KuukedLogoWhite width={80} height={40} />
-              </Center>
-              {menu && (
-                <TouchableOpacity
-                  onPress={() => {
-                    setMenuClicked(!menuClicked);
-                  }}>
-                  <MenuIcon />
-                </TouchableOpacity>
-              )}
-            </Queue>
-          )}
-          {menuClicked && navigation.navigate(NavigationRoutes.SideMenu)}
-        </Margin>
-      </Padding>
-    </Border>
+                {headerText && (
+                  <Text role="primary" type="primaryBody1" textAlign="center">
+                    {headerText}
+                  </Text>
+                )}
+                {menu && (
+                  <TouchableOpacity
+                    onPress={() => {
+                      handleMenuClick();
+                    }}>
+                    <MenuIcon />
+                  </TouchableOpacity>
+                )}
+              </Queue>
+            )}
+            {!withBack && (
+              <Queue justifyContent="space-between">
+                <Center>
+                  <KuukedLogoWhite width={80} height={40} />
+                </Center>
+                {menu && (
+                  <TouchableOpacity
+                    onPress={() => {
+                      handleMenuClick();
+                    }}>
+                    <MenuIcon />
+                  </TouchableOpacity>
+                )}
+              </Queue>
+            )}
+          </Margin>
+        </Padding>
+      </Border>
+      {menuClicked && <SideMenu />}
+    </>
   );
 };
--- src/components/header/sidemenu.tsx
+++ src/components/header/sidemenu.tsx
@@ -1,47 +1,50 @@
 import React, { FC } from 'react';
 import { TouchableOpacity, useWindowDimensions } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
 import { Queue } from '..';
+import {
+  EmergencyIcon,
+  HeartIcon,
+  MemoryIcon,
+  NewsIcon,
+  ProfileIcon,
+} from '../../assets';
 import { Border, Text } from '../core';
 import { Overlay, Padding, Stack } from '../layout';
-import { SafeAreaScreen } from '../screen';
-import { Header } from './header';
-const SideMenuItems = ({ text }) => {
+const SideMenuItems = ({ text, icon }) => {
   return (
     <TouchableOpacity
       onPress={() => {
         console.log('asd');
       }}>
-      <Padding size={[4, 3, 2, 2]}>
-        <Queue justifyContent="flex-start">
-          <Padding size={[3]}>
+      <Border bottomWidth="xlight" role="primary">
+        <Padding size={[3, 3, 3, 2]}>
+          <Queue size={6} justifyContent="flex-start" alignItems="center">
+            {icon}
             <Text textAlign="center" role="light">
               {text}
             </Text>
-          </Padding>
-        </Queue>
-      </Padding>
+          </Queue>
+        </Padding>
+      </Border>
     </TouchableOpacity>
   );
 };
 export const SideMenu: FC = () => {
   return (
-    <SafeAreaView>
-      <Header withBack={true} headerText="" />
-      <Overlay
-        width={useWindowDimensions().width / 2}
-        height={useWindowDimensions().height}
-        top={8}
-        right={0}
-        role="info">
-        <Stack size={3}>
-          <SideMenuItems text="Профайл" />
-          <SideMenuItems text="Мэдээлэл" />
-          <SideMenuItems text="Яаралтай тусламж" />
-          <SideMenuItems text="Дурсамж" />
-          <SideMenuItems text="Хадгалсан" />
-        </Stack>
-      </Overlay>
-    </SafeAreaView>
+    <Overlay
+      width={useWindowDimensions().width / 2}
+      height={useWindowDimensions().height}
+      zIndex={99}
+      top={8}
+      right={0}
+      role="info">
+      <Stack size={3}>
+        <SideMenuItems text="Профайл" icon={<ProfileIcon />} />
+        <SideMenuItems text="Мэдээлэл" icon={<NewsIcon />} />
+        <SideMenuItems text="Яаралтай тусламж" icon={<EmergencyIcon />} />
+        <SideMenuItems text="Дурсамж" icon={<MemoryIcon />} />
+        <SideMenuItems text="Хадгалсан" icon={<HeartIcon />} />
+      </Stack>
+    </Overlay>
   );
 };
